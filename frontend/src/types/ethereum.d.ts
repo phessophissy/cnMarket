9f4c75180f4d970fb9dd9cf1f2002f4ab73f2c08
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 19:27:44 +0100

    feat: add Ethereum provider type declarations for MiniPay

---
+++ frontend/src/types/ethereum.d.ts
@@ -0,0 +1,11 @@
+interface EthereumProvider {
+  isMiniPay?: boolean;
+  isMetaMask?: boolean;
+  request: (args: { method: string; params?: unknown[] }) => Promise<unknown>;
+  on: (event: string, handler: (...args: unknown[]) => void) => void;
+  removeListener: (event: string, handler: (...args: unknown[]) => void) => void;
+}
+
+interface Window {
+  ethereum?: EthereumProvider;
+}

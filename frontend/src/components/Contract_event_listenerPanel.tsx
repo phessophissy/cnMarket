ebd98393036721bb95e7416c30022c9e04139490
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 20:18:59 +0100

    feat: add Panel component for contract-event-listener

---
+++ frontend/src/components/Contract_event_listenerPanel.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+// feat: add contract event listener hook Panel
+export const Contract_event_listenerPanel = () => (
+  <div className="minipay-panel">feat: add contract event listener hook</div>
+);
+export default Contract_event_listenerPanel;

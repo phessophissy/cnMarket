2b7843284e4e81a5091019446d9ae9cbe99a579a
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 20:18:46 +0100

    feat: add Panel component for marketplace-listing

---
+++ frontend/src/components/Marketplace_listingPanel.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+// feat: add marketplace listing component Panel
+export const Marketplace_listingPanel = () => (
+  <div className="minipay-panel">feat: add marketplace listing component</div>
+);
+export default Marketplace_listingPanel;

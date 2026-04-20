a4e18d40b39b861e5b34fc8811040f0ee707453d
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 17:12:55 +0100

    refactor: add barrel export file for all components

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,52 @@
+// Layout
+export { Container } from "./Container";
+export { PageHeader } from "./PageHeader";
+export { Card } from "./Card";
+export { NFTGrid } from "./NFTGrid";
+export { Divider } from "./Divider";
+
+// Navigation
+export { Navbar } from "./Navbar";
+export { Footer } from "./Footer";
+export { Breadcrumb } from "./Breadcrumb";
+export { BackToTop } from "./BackToTop";
+
+// Display
+export { NFTCard, NFTCardSkeleton } from "./NFTCard";
+export { RarityBadge } from "./RarityBadge";
+export { NetworkBadge } from "./NetworkBadge";
+export { Badge } from "./Badge";
+export { Avatar } from "./Avatar";
+export { TokenIdBadge } from "./TokenIdBadge";
+export { StatusDot } from "./StatusDot";
+export { AddressDisplay } from "./AddressDisplay";
+export { WalletInfo } from "./WalletInfo";
+
+// Feedback
+export { Spinner } from "./Spinner";
+export { Notification } from "./Notification";
+export { Alert } from "./Alert";
+export { EmptyState } from "./EmptyState";
+export { TransactionStatus } from "./TransactionStatus";
+export { ProgressBar } from "./ProgressBar";
+export { Skeleton, SkeletonText } from "./Skeleton";
+export { Tooltip } from "./Tooltip";
+
+// Input
+export { Button } from "./Button";
+export { PriceInput } from "./PriceInput";
+export { SearchBar } from "./SearchBar";
+export { SortSelect } from "./SortSelect";
+export { RarityFilter } from "./RarityFilter";
+export { CopyButton } from "./CopyButton";
+
+// Overlay
+export { Modal } from "./Modal";
+
+// Misc
+export { ExternalLink } from "./ExternalLink";
+export { ConnectPrompt } from "./ConnectPrompt";
+export { Tabs } from "./Tabs";
+export { Countdown } from "./Countdown";
+export { SkipToContent } from "./SkipToContent";
+export { VisuallyHidden } from "./VisuallyHidden";

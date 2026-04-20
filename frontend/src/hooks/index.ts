1d87eaeec237e0dc6e60d70cca4ff805a017a394
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 20:28:51 +0100

    feat: add useMiniPay hook for MiniPay wallet detection (#1)
    * feat: create useMiniPay hook with wallet detection
    
    * feat: add useMiniPayTransaction hook for raw tx
    
    * feat: add Ethereum provider type declarations for MiniPay
    
    * feat: export MiniPay hooks from barrel file
    
    * feat: add MiniPay utility functions
    
    * feat: export MiniPay utils from lib barrel
    
    * feat: add MiniPay-related constants
    
    * feat: add useMiniPayStatus for environment detection
    
    * chore: export useMiniPayStatus hook
    
    * chore: add test plan for useMiniPay hook

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -7,3 +7,6 @@ export { useMediaQuery, useIsMobile } from "./useMediaQuery";
 export { useLocalStorage } from "./useLocalStorage";
 export { useDebounce } from "./useDebounce";
 export { useClipboard } from "./useClipboard";
+export { useMiniPay } from "./useMiniPay";
+export { useMiniPayTransaction } from "./useMiniPayTransaction";
+export { useMiniPayStatus } from "./useMiniPayStatus";

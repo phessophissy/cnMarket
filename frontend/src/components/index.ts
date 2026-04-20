2aa0967e636e6ffafe2c1303d7133ad9cd803d5b
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-20 19:28:15 +0100

    chore: export MiniPay components from barrel

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -7,6 +7,8 @@ export { Divider } from "./Divider";
 
 // Navigation
 export { Navbar } from "./Navbar";
+export { MiniPayBadge } from "./MiniPayBadge";
+export { MiniPayBanner } from "./MiniPayBanner";
 export { Footer } from "./Footer";
 export { Breadcrumb } from "./Breadcrumb";
 export { BackToTop } from "./BackToTop";
